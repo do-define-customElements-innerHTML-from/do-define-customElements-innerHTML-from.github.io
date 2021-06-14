4a086d1c2c852e9713ae2776a1a0ca6c40fe2a5a
Danny Engelman <danny@engelman.nl>
2021-06-14 13:47:01 +0200

    secret #1

---
+++ element.js
@@ -0,0 +1,20 @@
+let d = document.currentScript.src.split`-`;
+
+// let d=""
+// window[d[2]][d[1]](
+//   "secret-component",
+//   class extends window[d[9]] {
+//     connectedCallback() {
+//       this[d[3]] = d;
+//     }
+//   }
+// );
+
+// window.customElements.define(
+//   "secret-component",
+//   class extends window.HTMLElement {
+//     connectedCallback() {
+//       this.innerHTML = "";
+//     }
+//   }
+// );
